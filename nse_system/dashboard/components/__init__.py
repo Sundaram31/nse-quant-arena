12509bb20c098684707ba72c22d0979e8acc3186
Shanmuga sundaram <sundaram@Shanmugas-MacBook-Air.local>
2026-08-22 22:02:30 +0530

    Export plot_backtest_trades_chart in dashboard components __init__.py

--- nse_system/dashboard/components/__init__.py
+++ nse_system/dashboard/components/__init__.py
@@ -3,7 +3,8 @@ from nse_system.dashboard.components.charts import (
     plot_rrg_chart,
     plot_options_oi,
     plot_equity_curve,
-    plot_stock_strategy_chart
+    plot_stock_strategy_chart,
+    plot_backtest_trades_chart
 )
 from nse_system.dashboard.components.metrics_view import (
     render_kpi_cards,
@@ -16,6 +17,7 @@ __all__ = [
     'plot_options_oi',
     'plot_equity_curve',
     'plot_stock_strategy_chart',
+    'plot_backtest_trades_chart',
     'render_kpi_cards',
     'render_regime_banner',
     'render_trade_log_table'
